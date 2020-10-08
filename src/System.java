ab9344660faf95d274a437544c09a2fccafbf8a9
TylerLangtry <tylerlangtry@live.com>
2020-10-08 17:04:00 +1000

    Add scheduled executor to emulate the periodic tasks of the device

--- src/System.java
+++ src/System.java
@@ -1,4 +1,6 @@
 import java.io.PrintWriter;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class System {
 
@@ -9,11 +11,57 @@ public class System {
         Pump pump = new Pump();
         Sensor sensor = new Sensor();
 
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(10);
 
-        sensor.measureBloodData(blood);
-        controller.receiveBloodData(sensor.sendBloodData());
-        pump.receiveCommand(controller.sendInsulinInjection());
-        pump.injectInsulin(blood);
-        sensor.measureBloodData(blood);
+        // Measure blood sugar every x seconds and send to controller
+        executorService.scheduleAtFixedRate((Runnable) () -> {
+
+            // Task processes go here
+            java.lang.System.out.println("[" + new Date() + "] " + "Measure Blood Sugar");
+            sensor.measureBloodData(blood);
+            controller.receiveBloodData(sensor.sendBloodData());
+            java.lang.System.out.println();
+
+        }, 0, 2, TimeUnit.SECONDS);  // execute every x seconds
+
+        // Calculate required insulin every x seconds and send to pump for injection
+        executorService.scheduleAtFixedRate((Runnable) () -> {
+
+            // Task processes go here
+            java.lang.System.out.println("[" + new Date() + "] " + "Inject Insulin");
+            pump.receiveCommand(controller.sendInsulinInjection());
+            pump.injectInsulin(blood);
+            java.lang.System.out.println();
+
+        }, 5, 5, TimeUnit.SECONDS);  // execute every x seconds
+
+        // Send data to database every x seconds
+        executorService.scheduleAtFixedRate((Runnable) () -> {
+
+            // Task processes go here
+            java.lang.System.out.println("[" + new Date() + "] " + "Send to DB");
+            java.lang.System.out.println();
+
+        }, 0, 10, TimeUnit.SECONDS);  // execute every x seconds
+
+        //Code for shutting down executor service
+//**********************************************************************************************************************
+//        try {
+//            java.lang.System.out.println("attempt to shutdown executor");
+//            executorService.shutdown();
+//            executorService.awaitTermination(5, TimeUnit.SECONDS);
+//        }
+//        catch (InterruptedException e) {
+//            java.lang.System.err.println("tasks interrupted");
+//        }
+//        finally {
+//            if (!executorService.isTerminated()) {
+//                java.lang.System.err.println("cancel non-finished tasks");
+//            }
+//            executorService.shutdownNow();
+//            java.lang.System.out.println("shutdown finished");
+//        }
+//**********************************************************************************************************************
+
     }
 }
