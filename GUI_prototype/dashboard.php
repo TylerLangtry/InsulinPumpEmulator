7e5fb96b7ca27019e530967beadb5253325ed5b8
reynoldnepo <37422844+reynoldnepo@users.noreply.github.com>
2020-10-31 15:03:41 +1000

    Dashboard linechart, configuration, logout and reset pass

--- GUI_prototype/dashboard.php
+++ GUI_prototype/dashboard.php
@@ -17,15 +17,21 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="line_chart.js"></script>
+
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center; }
     </style>
+
 </head>
 <body>
     <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="mySidebar">
         <button class="w3-bar-item w3-button w3-large"
         onclick="sidebar_close()">Close &times;</button>
-        <a href="#" class="w3-bar-item w3-button">Link 1</a>
+        <a href="dashboard.php" class="w3-bar-item w3-button">Dashboard</a>
+        <a href="configuration.php" class="w3-bar-item w3-button">Configuration</a>
         <a href="reset-password.php" class="w3-bar-item w3-button">Reset Your Password</a>
         <a href="logout.php" class="w3-bar-item w3-button">Sign Out of Your Account</a>
     </div>
@@ -41,15 +47,32 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             </div>
         </div>
 
-        <div class="w3-container">
+        <!-- <div class="w3-container">
             <p>In this example, the sidebar is hidden (style="display:none")</p>
-         </div>
+        </div> -->
+
+
+        <label for="battery_progress">Battery level:</label>
+        <progress id="battery_progress" value="32" max="100"> 32% </progress>
+        <br>
+        <label for="insulin_progress">Insulin Reserve:</label>
+        <progress id="insulin_progress" value=40 max="100"> 40% </progress>
+
+        <br>
+
+        <div id="curve_chart" style="width: 900px; height: 500px"></div>
+
+        <br>
+
+        <h1> Past Alerts </h1>
+
+        <div >
+            <p id="alarm_box">text here </p>
+        </div>
     </div>
 
 
     <script>
-
-
     function sidebar_open() {
     document.getElementById("main").style.marginLeft = "25%";
     document.getElementById("mySidebar").style.width = "25%";
